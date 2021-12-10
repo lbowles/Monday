95d3412cb0220287abca93a7326494b23f6a22c6
lbowles <lspbowles@gmail.com>
2021-12-10 21:22:28 +0200

    adding intro lottie animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import background from './img/background.svg'
 import Section1 from './Components/Section1';
 import './App.css';
+import Lottie from 'react-lottie';
+import data from './data.json';
+
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: data,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }};
   return (
     <div className="App">
       <header className="container">
-        <img src={background} alt="background" className="background"></img>
+        <Lottie 
+        options={defaultOptions}
+        />
       </header>
       <Section1></Section1>
     </div>
